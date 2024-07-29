656b8e6ecc049bf9fa03f61ac99a277b2f9789a3
PCyrian <cyrian.pittaluga@epitech.eu>
2024-07-29 10:07:28 +0200

    ADD; relevant research papers to be reviewed for missing features

---
+++ features/faceswap.py
@@ -0,0 +1,1 @@
+#TO RESEARCH : https://docs.opencv.org/3.4/d1/d52/tutorial_face_swapping_face_landmark_detection.html
---
+++ features/lipsync.py
@@ -0,0 +1,1 @@
+# TO RESEARCH : https://github.com/synchronicity-labs/translation-starter?tab=MIT-1-ov-file
